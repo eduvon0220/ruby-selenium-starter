cedf1667b75bd607c45991f5473884db49d482c0
Eric Duvon <eric@chie.do>
2015-02-17 12:14:47 -0500

    add commenting to example files

--- tests/example_a.rb
+++ tests/example_a.rb
@@ -1,16 +1,21 @@
+# An example test to show how to structure your tests
+
 require "./tests/base_test.rb"
 
 class Test < BaseTest
 
   def initialize(driver, base_url, filename)
+  	# inherit initialization variables from super class (base_test.rb)
   	super(driver, base_url, filename)
   end
 
   def run()
+  	# Runs the tests. This is what will be getting called by run.rb
     @driver.navigate.to(@base_url + "/ncr")
 		element = @driver.find_element(:name, 'q')
 		element.send_keys "BrowserStack"
 		element.submit
 		puts @driver.title
+		# No need to quit driver at the end of the test. The run.rb file will handle that.
   end
 end
--- tests/example_b.rb
+++ tests/example_b.rb
@@ -1,3 +1,5 @@
+# An example test to show how to structure your tests
+
 require "./tests/base_test.rb"
 
 class Test < BaseTest
@@ -7,6 +9,7 @@ class Test < BaseTest
   end
 
   def run()
-    
+    # Runs the tests. This is what will be called by run.rb
+    # No need to quit the driver at end of test. The run.rb file will handle that
   end
 end
